9c980b96b5d6d17bd69b5d6fc2e013c44a92200f
Cássio Figueiredo <cassio.figueiredof@gmail.com>
2021-10-22 21:41:47 -0300

    Update GetLastGames.controller.ts

--- src/controllers/GetLastGames.controller.ts
+++ src/controllers/GetLastGames.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
 import { ControllerCommand } from "../interfaces/Command";
-import { GetLastGames } from "../services/GetLastGames.service";
+import { ReadJson } from "../utils/ReadJson";
 
 export class GetLastGamesController implements ControllerCommand {
     async handle(request: Request, response: Response) {
-        const getLastGames = new GetLastGames()
-        const lastGames = await getLastGames.execute()
+        const readJson = new ReadJson()
+        const lastGames = await readJson.execute("src/data/LastGames.json")
         
-        return response.json(lastGames)
+        return response.json(lastGames['games'])
     }
 }
