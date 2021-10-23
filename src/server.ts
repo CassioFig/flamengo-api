8b8ec66be4ae3df6f713c6d1c8f95d78639cd86a
Cássio Figueiredo <cassio.figueiredof@gmail.com>
2021-10-23 17:37:17 -0300

    swagger e logger

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+// app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(router)
 
 logger.info('Running Jobs')
